8378a8caf13ebf9a2f4810e040b90dafd84bc35f
AngusmMc <amac0028@student.monash.edu>
2025-01-01 21:42:19 +1100

    Delete unwanted file

--- downloadIt.py
+++
@@ -1,5 +0,0 @@
-import requests # allows us to download files from the web
-res = requests.get("https://automatetheboringstuff.com/files/rj.txt")
-print(type(res))
-print(len(res.text))
-print(res.text)
